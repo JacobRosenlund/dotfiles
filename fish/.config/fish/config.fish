8dbc3f3dce6e9682f08a3c52823163f54a0b0e82
JacobRosenlund <jakemrosenlund@gmail.com>
2025-02-18 23:05:54 -0700

    Added various aliases
    Added:
    - ntfy-rebuild (May rename to rebuild-ntfy)
    
    Replaced:
    - cat with bat
    
    Configured:
    - fzf tab-completion

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -2,5 +2,15 @@ eval "$(starship init fish)"
 set SSH_AUTH_SOCK /tmp/ssh-XXXXXXfxf9XP/agent.35422
 
 # Aliases
-alias ls "eza --icons"
-alias la "eza -a --icons"
+  # LS and LA
+  alias ls "eza --icons"
+  alias la "eza -a --icons"
+
+  # Nixos rebuild
+  alias ntfy-rebuild "sudo nixos-rebuild switch --flake /etc/nixos/#default && curl -d \"✅ Nix rebuild done\" ntfy.sh/NixOS_alerts || curl -d \"⚠️ Nix rebuild failed!\""
+
+  # Bat cat
+  alias cat "bat --color=always"
+
+# FZF Tab-complete
+fzf_configure_bindings --directory=\t --variables=\e\cv
