79e72efb0401e85faceac6d7f44dec28cac3f476
JacobRosenlund <jakemrosenlund@gmail.com>
2025-09-11 16:46:45 -0600

    Changed a lot of my eww scripts

--- eww/.config/eww/scripts/utils/clamp.py
+++
@@ -1,6 +0,0 @@
-def clamp (n, minimum, maximum):
-    if n < minimum:
-        return minimum
-    elif n > maximum:
-        return maximum
-    return n
--- eww/.config/eww/scripts/utils/eww_object_generator.py
+++
@@ -1,9 +0,0 @@
-def create_button (classname, onclick, content):
-    return f"(button :class \"{classname}\" :onclick \"{onclick}\" \"{content}\")"
-
-
-def create_box (preamble, classname, content, quote=True):
-    if quote:
-        return f"(box {preamble} :class \"{classname}\" \"{content}\")"
-
-    return f"(box {preamble} :class \"{classname}\" {content})"
